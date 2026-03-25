d0e498bf1cd66848dfd66519aa498c64b2397bf2
Sean Walsh <seanmichwalsh@protonmail.ch>
2026-03-25 02:03:11 -0700

    plugins/lspconfig: Add support for Mason package manager
    Add Mason LSP package management plugin. Auto configure all
    Mason-installed LSPs by default.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,6 +3,8 @@
 --
 -- Collection of default configs for LSP servers provided by the Neovim team.
 -- Integrates with Neovim's native LSP framework as of version >=0.11.
+---
+--- Depends: nil
 
 vim.pack.add({'https://github.com/neovim/nvim-lspconfig.git'})
 
@@ -15,4 +17,27 @@ local system_lsps = {
 }
 vim.lsp.enable(system_lsps)
 
+-- mason.nvim
+-- https://github.com/mason-org/mason.nvim.git
+--
+-- TUI-based package manager for acquiring LSPs and formatters, regardless of
+-- the system environment. Provides useful LSP user commands as well.
+--
+-- Depends: nil
+
+vim.pack.add({"https://github.com/mason-org/mason.nvim.git"})
+require('mason').setup()
+
+-- mason-lspconfig.nvim
+-- https://github.com/mason-org/mason-lspconfig.nvim.git
+--
+-- Glue that dynamically pipes LSPs installed via Mason into Neovim's native
+-- LSP setup APIs.
+--
+-- Depends: { nvim-lspconfig, mason.nvim }
+
+vim.pack.add({"https://github.com/mason-org/mason-lspconfig.nvim.git"})
+require('mason-lspconfig').setup()
+
+-- Keymaps for Neovim native LSP commands.
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
